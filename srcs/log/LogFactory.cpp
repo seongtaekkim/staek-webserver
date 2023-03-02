f11bd75c5a7df690eca98dad572db9fa5e789eb2
seongtaekkim <chxortnl@gmail.com>
2023-03-02 16:00:19 +0900

    fix : add logFactory comment

--- srcs/log/LogFactory.cpp
+++ srcs/log/LogFactory.cpp
@@ -1,8 +1,14 @@
 
 #include "LogFactory.hpp"
 #include "Logger.hpp"
-#include <utility>
 
+/**
+* @brief 요청인자에 해당하는 로그 컨테이너를 리턴한다.
+		컨테이너에 없다면, 생성 후 리턴한다.
+* @return Logger 
+* @param 로그 이름
+* @date 2023.03.21
+*/
 Logger&
 LogFactory::get(const std::string &tag)
 {
@@ -14,6 +20,10 @@ LogFactory::get(const std::string &tag)
 	return (*it->second);
 }
 
+/**
+* @brief 로그 컨테이너 자원해제
+* @date 2023.03.21
+*/
 void
 LogFactory::destroy(void)
 {
@@ -24,10 +34,15 @@ LogFactory::destroy(void)
 	storage.clear();
 }
 
+/**
+* @brief 정적 로그map 을 생성한다.
+* @return LogFactory::logger_map&
+* @param 로그 이름
+* @date 2023.03.21
+*/
 LogFactory::logger_map&
 LogFactory::getStorage()
 {
 	static logger_map storage;
-
 	return (storage);
 }
--- srcs/log/LogFactory.hpp
+++ srcs/log/LogFactory.hpp
@@ -3,18 +3,23 @@
 #include <map>
 #include <string>
 
+
+
+/**
+ * @brief log 컨테이너를 관리하는 객체이다.
+ * @details 클래스별 로그배열을 관리한다.
+ * @author seongtki
+ * @date 2023.03.21
+ */
 class LogFactory {
 public:
-	typedef std::map<std::string, Logger*> logger_map;
-	typedef logger_map::iterator iterator;
+	typedef std::map<std::string, Logger*>	logger_map;
+	typedef logger_map::iterator			iterator;
 
 private:
 	static logger_map &getStorage();
 
 public:
-	static Logger&
-	get(const std::string &tag);
-
-	static void
-	destroy(void);
+	static Logger& get(const std::string &tag);
+	static void destroy(void);
 };
