079d38680abc292e6ac774443586c3c32805f2b8
seongtaekkim <chxortnl@gmail.com>
2023-03-29 16:41:52 +0900

    add: http syntex

---
+++ srcs/http/SHTTP.cpp
@@ -0,0 +1,4 @@
+#include "SHTTP.hpp"
+
+const std::string SHTTP::SP = " ";
+const std::string SHTTP::CRLF = "\r\n";
---
+++ srcs/http/SHTTP.hpp
@@ -0,0 +1,12 @@
+#ifndef SHTTP_HPP
+#define SHTTP_HPP
+
+#include <string>
+
+class SHTTP {
+public:
+	static const std::string SP;
+	static const std::string CRLF;
+};
+
+#endif
