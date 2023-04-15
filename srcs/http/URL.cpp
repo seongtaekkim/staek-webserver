9642bebfc372bb1106aaa228c5ef99bb1ce9005e
seongtaekkim <chxortnl@gmail.com>
2023-04-15 21:38:42 +0900

    add : url class

---
+++ srcs/http/URL.cpp
@@ -0,0 +1,184 @@
+#include "URL.hpp"
+
+URL::URL() : _path(), _queryParameters(), _fragment() {
+	std::cout << "constructor" << std::endl;
+}
+
+URL::URL(const std::string &path, const std::map<std::string, std::string> queryParameters, const std::string fragment) :
+		_path(path), _queryParameters(queryParameters), _fragment(fragment) {
+	std::cout << "constructor with param" << std::endl;
+}
+
+URL::URL(const URL &other) :
+		_path(other._path), _queryParameters(other._queryParameters), _fragment(other._fragment) {
+	std::cout << "copy constructor" << std::endl;
+}
+
+URL::~URL() {}
+
+URL& URL::operator= (const URL &other) {
+	if (this != &other) {
+		_path = other._path;
+		_queryParameters = other._queryParameters;
+		_fragment = other._fragment;
+	}
+	return (*this);
+}
+
+const std::string& URL::path() const {
+	return (_path);
+}
+
+const std::map<std::string, std::string>& URL::queryParameters() const {
+	return _queryParameters;
+}
+
+const std::string& URL::fragment() const {
+	return (_fragment);
+}
+
+std::string URL::fullUrl(void) const {
+	std::string out = _path;
+
+	if (_queryParameters.size() > 0)
+		out += "?" + queryString();
+	if (_fragment.size() > 0)
+		out += "#" + _fragment;
+
+	return (out);
+}
+
+std::string
+URL::queryString(void) const {
+	std::string out;
+
+	if (_queryParameters.size() > 0) {
+		typedef std::map<std::string, std::string> map;
+
+		const map &queryParams = _queryParameters;
+
+		for (map::const_iterator it = queryParams.begin(); it != queryParams.end(); it++)
+		{
+			out += encode(it->first) + "=" + encode(it->second);
+			out += "&";
+		}
+	}
+
+	return (out);
+}
+
+bool
+URL::filename(std::string &out) const {
+	if (_path.empty())
+		return (false);
+
+	std::string::size_type lastSlashPos = _path.rfind("/");
+
+	if (lastSlashPos != std::string::npos)
+		out = _path.substr(lastSlashPos + 1);
+	else
+		out = _path;
+
+	return (true);
+}
+
+bool
+URL::extension(std::string &out) const
+{
+	std::string filename;
+	if (!URL::filename(filename))
+		return (false);
+
+	std::string::size_type lastDotPos = filename.rfind(".");
+
+	if (lastDotPos == std::string::npos)
+		return (false);
+
+	out = filename.substr(lastDotPos + 1);
+
+	return (true);
+}
+
+URL::Builder
+URL::builder(void) const {
+	return (Builder(*this));
+}
+
+URL::Builder::Builder() :
+		_path(), _queryParameters(), _fragment() {
+}
+
+URL::Builder::Builder(const URL &url) :
+		_path(url._path),
+		_queryParameters(url._queryParameters),
+		_fragment(url._fragment)
+{
+}
+
+URL::Builder::Builder(const Builder &other) :
+		_path(other._path),
+		_queryParameters(other._queryParameters),
+		_fragment(other._fragment)
+{}
+
+URL::Builder::~Builder() {}
+
+URL::Builder&
+URL::Builder::operator =(const Builder &other)
+{
+	if (this != &other)
+	{
+		_path = other._path;
+		_queryParameters = other._queryParameters;
+		_fragment = other._fragment;
+	}
+
+	return (*this);
+}
+
+URL::Builder&
+URL::Builder::path(const std::string &path)
+{
+	_path = path;
+
+	return (*this);
+}
+
+URL::Builder&
+URL::Builder::appendPath(const std::string &content)
+{
+	_path += content;
+	return (*this);
+}
+
+URL::Builder&
+URL::Builder::queryParameters(const std::map<std::string, std::string> &queryParameters) {
+	_queryParameters = queryParameters;
+	return (*this);
+}
+
+URL::Builder& URL::Builder::fragment(const std::string &fragment) {
+	_fragment = fragment;
+	return (*this);
+}
+
+URL URL::Builder::build() {
+	return (URL(_path, _queryParameters, _fragment));
+}
+
+std::string URL::encode(const std::string &input) {
+	return (input);
+}
+
+int main(void) {
+	std::map<std::string, std::string> query;
+	query["v"] = "QsuhIGOXxfs";
+	query["user"] = "seongtki";
+	
+	URL u = URL().builder().appendPath("https://www.youtube.com/watch").queryParameters(query).fragment("hash").build();
+	std::cout << u.path() << std::endl;
+	std::cout << u.queryString() << std::endl;
+	std::cout << u.fragment() << std::endl;
+	std::cout << u.fullUrl() << std::endl;
+	// system("leaks a.out");
+}
---
+++ srcs/http/URL.hpp
@@ -0,0 +1,58 @@
+#ifndef URL_HPP
+#define URL_HPP
+
+#include <map>
+#include <string>
+#include <vector>
+#include <iostream>
+
+class URL {
+	public:
+		class Builder;
+
+	private:
+		std::string _path;
+		std::map<std::string, std::string> _queryParameters;
+		std::string _fragment;
+
+	public:
+		URL();
+		URL(const std::string &path, const std::map<std::string, std::string> queryParameters, const std::string fragment);
+		URL(const URL &other);
+		URL& operator =(const URL &other);
+		virtual ~URL();
+
+		const std::string& path() const;
+		const std::map<std::string, std::string>& queryParameters() const;
+		const std::string& fragment() const;
+		std::string fullUrl(void) const;
+		std::string queryString(void) const;
+		bool filename(std::string &out) const;
+		bool extension(std::string &out) const;
+		Builder builder(void) const;
+	public:
+		static std::string encode(const std::string &input);
+
+	public:
+		class Builder {
+			private:
+				std::string _path;
+				std::map<std::string, std::string> _queryParameters;
+				std::string _fragment;
+
+			public:
+				Builder(void);
+				Builder(const URL &url);
+				Builder(const Builder &other);
+
+				virtual ~Builder();
+				Builder& operator=(const Builder &other);
+				Builder& path(const std::string &path);
+				Builder& appendPath(const std::string &content);
+				Builder& queryParameters(const std::map<std::string, std::string> &queryParameters);
+				Builder& fragment(const std::string &fragment);
+				URL build();
+		};
+};
+
+#endif
