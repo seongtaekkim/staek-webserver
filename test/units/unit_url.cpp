555017268082fede2b0d7da0e6334f1fb70c9649
seongtaekkim <chxortnl@gmail.com>
2023-04-18 11:31:38 +0900

    test : update

---
+++ test/units/unit_url.cpp
@@ -0,0 +1,12 @@
+// int main(void) {
+// 	std::map<std::string, std::string> query;
+// 	query["v"] = "QsuhIGOXxfs";
+// 	query["user"] = "seongtki";
+	
+// 	URL u = URL().builder().appendPath("https://www.youtube.com/watch").queryParameters(query).fragment("hash").build();
+// 	std::cout << u.path() << std::endl;
+// 	std::cout << u.queryString() << std::endl;
+// 	std::cout << u.fragment() << std::endl;
+// 	std::cout << u.fullUrl() << std::endl;
+// 	// system("leaks a.out");
+// }
