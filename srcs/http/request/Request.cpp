9505b9fd0875e53f8c370232693e2455fb628bda
seongtaekkim <chxortnl@gmail.com>
2023-04-28 22:08:07 +0900

    update : add request value (url)

--- srcs/http/request/Request.cpp
+++ srcs/http/request/Request.cpp
@@ -6,10 +6,31 @@ typedef std::map<std::string, std::vector<std::string> >	headers_table;
 	OCCF
 */
 
-Request::Request() {}
+Request::Request(void) : _resource() {}
 
-Request::Request(const Request& __copy)
-{
+Request::Request(const StatusLine& statusLine, const URL& url) :
+		_statusLine(statusLine),
+		_url(url),
+		// _header(headers),
+		// _method(),
+		// _serverBlock(),
+		// _locationBlock(),
+		_resource(url.path())
+{}
+
+// Request::Request(const StatusLine& statusLine, const URL& url, const Header& header) :
+// 		_statusLine(statusLine),
+// 		_url(url),
+// 		// _header(headers),
+// 		_method(),
+// 		_serverBlock(),
+// 		_locationBlock(),
+// 		_resource(url.path())
+// {
+// }
+
+
+Request::Request(const Request& __copy) : _resource(__copy._resource) {
 	_reqString = __copy._reqString;
 	_requestLine = __copy._requestLine;
 	_headers = __copy._headers;
@@ -20,6 +41,7 @@ Request	Request::operator=(const Request& __copy)
 {
 	if (this != &__copy)
 	{
+		this->_url = __copy._url;
 		_reqString = __copy._reqString;
 		_requestLine = __copy._requestLine;
 		_headers = __copy._headers;
@@ -53,6 +75,41 @@ RequestHeaders	Request::getHeaders() { return (_headers); }
 e_method		Request::method() { return (_requestLine._method); }
 std::string		Request::uri() { return (_requestLine._uri); }
 float			Request::version() { return (_requestLine._version); }
-headers_table	Request::headers() { return (_headers._headers); }
+headers_table	Request::headers() { return (_headers._headers); } 
 std::string		Request::body() { return (_body); }
 size_t			Request::header_count() { return (_headers._headerCount); }
+
+
+File Request::targetFile() {
+	// _resource = "/";
+	_resource = "/Makefile";
+	return (File(root(), _resource));
+}
+
+std::string Request::root(void) const {
+	// if (locationBlock().present())
+	// {
+	// 	const LocationBlock &locationBlock = *this->locationBlock().get();
+
+	// 	if (locationBlock.root().present())
+	// 		return (locationBlock.root().get());
+	// }
+
+	// if (serverBlock().present())
+	// {
+	// 	const ServerBlock &serverBlock = *this->serverBlock().get();
+
+	// 	if (serverBlock.root().present())
+	// 		return (serverBlock.root().get());
+	// }
+
+	// if (Configuration::instance().rootBlock().root().present())
+	// 	return (Configuration::instance().rootBlock().root().get());
+	// return (File::currentDirectory().path());
+	
+	return (File::currentDir());
+}
+
+URL Request::url(void) {
+	return (this->_url);
+}
--- srcs/http/request/Request.hpp
+++ srcs/http/request/Request.hpp
@@ -4,6 +4,13 @@
 #include "RequestParser.hpp"
 #include "RequestLine.hpp"
 #include "RequestHeaders.hpp"
+#include "../../file/File.hpp"
+#include "../response/StatusLine.hpp"
+#include "../URL.hpp"
+// #include "../response/method/Method.hpp"
+// #include "../../config/block/LocationBlock.hpp"
+// #include "../../config/block/ServerBlock.hpp"
+// #include "../Header.hpp"
 
 /**
  * @brief HTTP 요청 메시지 string을 파싱하여 저장
@@ -16,9 +23,26 @@ class Request {
 public:
 	typedef std::map<std::string, std::vector<std::string> >	headers_table;
 
+private:
+	std::string				_resource;
+	RequestString			_reqString;
+	RequestLine				_requestLine;
+	RequestHeaders			_headers;
+	std::string				_body;
+	bool					_isChunked;
+
+	StatusLine				_statusLine;
+	URL						_url;
+	// HTTPHeaderFields m_headers;
+	// const Method*			_method;
+	// const ServerBlock*		_serverBlock;
+	// const LocationBlock*	_locationBlock;
+
 public:
-	Request();
-	~Request();
+	Request(void);
+	Request(const StatusLine& statusLine, const URL& url);
+	// Request(const StatusLine& statusLine, const URL& url, const Header& header);
+	~Request(void);
 	Request(const Request& __copy);
 	Request	operator=(const Request& __copy);
 
@@ -35,12 +59,10 @@ public:
 	std::string		body();
 	size_t			header_count();
 
-private:
-	RequestString			_reqString;
-	RequestLine				_requestLine;
-	RequestHeaders			_headers;
-	std::string				_body;
-	bool					_isChunked;
+	File targetFile(void);
+	std::string root(void) const;
+
+	URL url(void);
 };
 
 #endif
