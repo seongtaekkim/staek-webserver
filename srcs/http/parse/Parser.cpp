88a8ba37ca522797e510f3d75bf51cdeca753004
seongtaekkim <chxortnl@gmail.com>
2023-04-30 11:15:37 +0900

    add : request parser

--- srcs/http/parse/Parser.cpp
+++ srcs/http/parse/Parser.cpp
@@ -1,5 +1,9 @@
 #include "Parser.hpp"
 
+Parser::Parser(void) : _state(Parser::NOT_STARTED), _pathParser() {}
+
+Parser::~Parser(void) {}
+
 std::vector<std::string>	Parser::split(std::string str, std::string delim)
 {
     std::vector<std::string>	tokens;
@@ -63,4 +67,260 @@ std::string Parser::toupper(std::string str)
             result += *it;
     }
     return result;
+}
+
+void Parser::parse(char c) {
+
+	switch (this->_state)
+	{
+		case Parser::NOT_STARTED:
+		{
+			if (c == '\r' || c == '\n')
+				break;
+			else
+				_state = Parser::METHOD;
+		}
+		case Parser::METHOD:
+		{
+			// m_max = false;
+			if (c == ' ')
+			{
+				if (_state == Parser::NOT_STARTED)
+					throw Exception("No method");
+				_state = Parser::SPACES_BEFORE_PATH;
+			}
+			else
+			{
+				// if (!ft::isupper(c))
+				// 	throw Exception("Method is only upper-case letter");
+
+				// if (_method.length() > 20)
+				// 	throw Exception("Method is over 20 characters");
+
+				_state = Parser::METHOD;
+				_method += c;
+			}
+
+			break;
+		}
+
+		case Parser::SPACES_BEFORE_PATH:
+		{
+			if (c != ' ')
+			{
+				if (c != '/')
+					throw Exception("No slash");
+
+				_state = Parser::PATH;
+			}
+
+			break;
+		}
+
+		case Parser::PATH:
+		{
+			_pathParser.parse(c);
+
+			if (_pathParser.state() == PathParser::S_END)
+				_state = Parser::HTTP_START;
+
+			break;
+		}
+
+		case Parser::HTTP_START:
+		{
+			if (c != 'H')
+				throw Exception("Expected `H`");
+
+			_state = Parser::HTTP_H;
+
+			break;
+		}
+
+		case Parser::HTTP_H:
+		{
+			if (c != 'T')
+				throw Exception("Expected `T`");
+
+			_state = Parser::HTTP_HT;
+
+			break;
+		}
+
+		case Parser::HTTP_HT:
+		{
+			if (c != 'T')
+				throw Exception("Expected `T`");
+
+			_state = Parser::HTTP_HTT;
+
+			break;
+		}
+
+		case Parser::HTTP_HTT:
+		{
+			if (c != 'P')
+				throw Exception("Expected `P`");
+
+			_state = Parser::HTTP_HTTP;
+
+			break;
+		}
+
+		case Parser::HTTP_HTTP:
+		{
+			if (c != '/')
+				throw Exception("Expected `/`");
+
+			_state = Parser::HTTP_SLASH;
+
+			break;
+		}
+
+		case Parser::HTTP_SLASH:
+		{
+			// if (!ft::isdigit(c))
+				// throw Exception("Expected a number");
+
+			// _major = c - '0';
+			_state = Parser::HTTP_MAJOR;
+
+			break;
+		}
+
+		case Parser::HTTP_MAJOR:
+		{
+			if (c != '.')
+				throw Exception("Expected `.`");
+
+			_state = Parser::HTTP_DOT;
+
+			break;
+		}
+
+		case Parser::HTTP_DOT:
+		{
+			// if (!ft::isdigit(c))
+				// throw Exception("Expected a number");
+
+			// _minor = c - '0';
+			_state = Parser::HTTP_MINOR;
+
+			break;
+		}
+
+		case Parser::HTTP_MINOR:
+		{
+			// if (!HTTPVersion::isSupported(m_minor, m_major))
+			// 	throw UnsupportedHTTPVersion::of(m_minor, m_major);
+
+			if (c == '\r')
+				_state = Parser::HTTP_END_R;
+			else if (c == '\n')
+				_state = Parser::HTTP_END_N;
+			else
+				throw Exception("Expected a \\r or \\n");
+
+			break;
+		}
+
+		case Parser::HTTP_END_R:
+		{
+			if (c == '\n')
+				_state = Parser::HTTP_END_N;
+			else
+				throw Exception("Expected a \\n");
+
+			break;
+		}
+
+		case Parser::HTTP_END_N:
+		{
+			if (c == '\r')
+				_state = Parser::END_R;
+			else if (c == '\n')
+				_state = Parser::END;
+			else
+			{
+				_state = Parser::HEADER_FIELDS;
+				// m_headerFieldsParser.consume(c);
+			}
+
+			break;
+		}
+
+		case Parser::END_R:
+		{
+			if (c == '\n')
+				_state = Parser::END;
+			else
+				_state = Parser::END;
+
+			break;
+		}
+
+		case Parser::HEADER_FIELDS:
+		{
+			// m_headerFieldsParser.consume(c);
+
+			// if (m_headerFieldsParser.state() == HTTPHeaderFieldsParser::S_END)
+				_state = Parser::END;
+
+			break;
+		}
+
+		case Parser::BODY:
+			_state = Parser::BODY_DECODE;
+			// _bodyDecoder = HTTPBodyEncoding::decoderFor(m_headerFieldsParser.headerFields());
+
+			// if (m_bodyDecoder == NULL)
+			// {
+			// 	_state = Parser::END;
+			// 	break;
+			// }
+
+			if (c == 0)
+				break;
+
+			// fall through
+
+		case Parser::BODY_DECODE:
+		{
+			// size_t consumed = 0;
+			// bool finished = m_bodyDecoder->consume(m_client.in().storage(), m_client.body(), consumed, m_max);
+
+			// m_client.in().skip(consumed);
+			// m_totalSize += consumed;
+
+			// if (m_maxBodySize != -1 && (long long)m_client.body().size() > m_maxBodySize) // TODO This kept everything in RAM...
+			// {
+			// 	m_max = true;
+			// 	if (finished)
+			// 		throw HTTPRequestPayloadTooLargeException();
+			// }
+
+			// if (finished)
+			// 	_state = Parser::END;
+
+			break;
+		}
+
+		case Parser::END:
+			break;
+
+	}
+}
+
+PathParser Parser::pathParser(void) const {
+    return (this->_pathParser);
+}
+
+
+std::string Parser::method(void) {
+	return (this->_method);
+}
+
+
+Parser::STATE Parser::state(void) const {
+	return (this->_state);
 }
--- srcs/http/parse/Parser.hpp
+++ srcs/http/parse/Parser.hpp
@@ -1,5 +1,8 @@
 #ifndef PARSER_HPP
 #define PARSER_HPP
+
+#include "PathParser.hpp"
+#include "../../exception/Exception.hpp"
 #include <vector>
 #include <string>
 
@@ -14,11 +17,45 @@
  * @date 2023.03.29
  */
 class Parser {
-public :
+public:
+	enum STATE  {
+		NOT_STARTED = 0,
+		METHOD,
+		SPACES_BEFORE_PATH,
+		PATH,
+		HTTP_START,
+		HTTP_H,
+		HTTP_HT,
+		HTTP_HTT,
+		HTTP_HTTP,
+		HTTP_SLASH,
+		HTTP_MAJOR,
+		HTTP_DOT,
+		HTTP_MINOR,
+		HTTP_END_R,
+		HTTP_END_N,
+		HEADER_FIELDS,
+		BODY,
+		BODY_DECODE,
+		END_R,
+		END
+	};
+private:
+	Parser::STATE	_state;
+	PathParser		_pathParser;
+	std::string		_method;
+public:
+	Parser(void);
+	~Parser(void);
 	static std::vector<std::string>		split(std::string str, std::string delim);
 	static std::vector<std::string>		split(std::string str, char delim);
 	static void							trim(std::string& str);
 	static std::string					toupper(std::string str);
+
+	void parse(char c);
+	PathParser pathParser(void) const;
+	std::string method(void);
+	Parser::STATE state(void) const;
 };
 
 #endif
