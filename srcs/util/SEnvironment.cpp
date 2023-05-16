d64ec4af906f7db02ef08ebf7693a6f286eab23a
seongtaekkim <chxortnl@gmail.com>
2023-05-16 18:33:11 +0900

    update : environment logic

--- srcs/util/SEnvironment.cpp
+++ srcs/util/SEnvironment.cpp
@@ -1,33 +1,91 @@
 #include "SEnvironment.hpp"
 
-SEnvironment::SEnvironment(void) {}
-SEnvironment::SEnvironment(EnvMap map) {}
-SEnvironment::SEnvironment(const SEnvironment& other) {}
-SEnvironment& SEnvironment::operator=(const SEnvironment& other) {}
+SEnvironment SEnvironment::_sEnv;
+
+SEnvironment::SEnvironment(void) : _env() {}
+
+SEnvironment::SEnvironment(EnvMap map) : _env(map) {}
+
+SEnvironment::SEnvironment(const SEnvironment& other) : _env(other._env) {}
+
+SEnvironment& SEnvironment::operator=(const SEnvironment& other) {
+	if (this != &other) {
+		this->_env = other._env;
+	}
+	return (*this);
+}
+
 SEnvironment::~SEnvironment(void) {}
+
+	char*
+	strchr2(char *str, char c)
+	{
+		if (str)
+		{
+			char x;
+			while ((x = *str++))
+				if (x == c)
+					return (--str);
+		}
+
+		return (NULL);
+	}
 
 /**
 * @brief os env 세팅
 * @param char **env
 * @date 2023.03.26
 */
-void SEnvironment::setEnv(char **env) {
-	// map<string, string>
-	SEnvironment env;
-	// _storage = e;
+SEnvironment SEnvironment::charToMap(char **env) {
+	SEnvironment environment;
+
+	char* key;
+	while ((key = *env)) {
+		char* equal = strchr2(key, '=');
+		std::string str = key;
+		char* value;
+		if (equal) {
+			equal[0] = '\0';
+			value = equal + 1; 
+		}
+		environment.appendOne(str.substr(0, str.find("=")), value);		
+		env++;
+	}
+	return (environment);
+}
+
+void SEnvironment::setEnv(char** envp) {
+	SEnvironment::_sEnv = SEnvironment::charToMap(envp);
+	// std::cout << _sEnv.size() << std::endl;
+}
+
+SEnvironment::EnvMap& SEnvironment::getMap(void) {
+	return (this->_env);
 }
 
 SEnvironment& SEnvironment::getEnv(void) {
-	return (SEnvironment::_s_env);
+	return (SEnvironment::_sEnv);
 }
 
 /**
 * @brief map to char**
 * @param char **env
-* @date 2023.03.26
+* @date 2023.05.15
 */
-char** envToEnvp(void) {
+char** SEnvironment::mapToChar(void) const {
+	char **envp = new char*[this->_env.size() + 1];
 
+	EnvMap::size_type index = 0;	
+	for (EnvMap::const_iterator it = this->_env.begin(); it != this->_env.end(); ++it) {
+		std::string line = it->first + '=' + it->second;
+		envp[index++] = ft::strdup(line.c_str());
+	}
+	envp[index] = NULL;
+	return (envp);
+}
+
+int SEnvironment::size(void) const {
+	return this->_env.size();
 }
 
 /**
@@ -35,6 +93,23 @@ char** envToEnvp(void) {
 * @param (key, value)
 * @date 2023.03.26
 */
-void setProperty(const std::string& key, const std::string& value) {
-	
+void SEnvironment::appendOne(const std::string& key, const std::string& value) {
+	EnvMap::iterator it = this->_env.find(key);
+
+	if (it == this->_env.end()) 
+		this->_env.insert(std::make_pair(key, value));
+	(*it).second = value;
+}
+
+std::string SEnvironment::getOne(const std::string& key) {
+	EnvMap::iterator it = this->_env.find(key);
+	if (it == this->_env.end())
+		return (std::string());
+	return ((*it).second);
+}
+
+bool SEnvironment::hasOne(const std::string& key) {
+	if (this->_env.find(key) == this->_env.end())
+		return (false);
+	return (true);
 }
--- srcs/util/SEnvironment.hpp
+++ srcs/util/SEnvironment.hpp
@@ -2,18 +2,23 @@
 #define SENVIRONMENT_HPP
 
 #include <map>
+#include "Common.hpp"
+#include <iostream>
 
 /**
  * @brief 환경변수 관리
  * @details os 환경변수 및 CGI 등의 사용자 환경변수 관리 객체
+ * 			_sEnv : os 환경변수 저장
+ * 			_env  : 추가 환경변수 관리변수
  * @author seongtki
  * @date 2023.03.25
  */
 class SEnvironment {
-private:
+public:
 	typedef std::map<std::string, std::string>	EnvMap;
-	EnvMap										_storage;
-	static SEnvironment							_s_env;
+private:
+	EnvMap										_env;
+	static SEnvironment							_sEnv;
 public:
 	SEnvironment(void);
 	SEnvironment(EnvMap map);
@@ -22,10 +27,17 @@ public:
 	~SEnvironment(void);
 
 public:
-	static void setEnv(char **env);
+	static SEnvironment charToMap(char **env);
+	static void setEnv(char** envp);
 	static SEnvironment& getEnv(void);
-	char** envToEnvp(void);
-	void setProperty(const std::string& key, const std::string& value);
+	SEnvironment::EnvMap& getMap(void);
+	char** mapToChar(void) const;
+	bool hasOne(const std::string& key);
+	void appendOne(const std::string& key, const std::string& value);
+	std::string getOne(const std::string& key);
+	int size(void) const;
 };
+
+
 
 #endif
