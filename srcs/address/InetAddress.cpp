e93b73476c40bec9a1581f35a1354f21daef237c
seongtaekkim <chxortnl@gmail.com>
2023-05-18 01:35:12 +0900

    update : ip address

--- srcs/address/InetAddress.cpp
+++ srcs/address/InetAddress.cpp
@@ -3,21 +3,40 @@
 InetAddress::InetAddress(void) {}
 InetAddress::~InetAddress(void) {}
 
-InetAddress::InetAddress(const InetAddress& other) : _address(other.address()), _port(other.port()) {
-
+InetAddress::InetAddress(const InetAddress& other) : _address(), _port(other.port()) {
+	std::memcpy(this->_address, other._address, sizeof(this->_address));
 }
+
 InetAddress& InetAddress::operator=(const InetAddress& other) {
 	if (&other != this) {
-		this->_address = other.address();
+		std::memcpy(this->_address, other._address, sizeof(this->_address));
 		this->_port = other.port();
 	}
 	return (*this);
 }
 
-int InetAddress::port() const {
+int InetAddress::port(void) const {
 	return (this->_port);
 }
-std::string InetAddress::address() const {
-	return (this->_address);
+
+InetAddress::InetAddress(unsigned int address, int port) {
+	std::memcpy(this->_address, (unsigned char*)&address, sizeof(this->_address));
+	this->_port = port;
 }
 
+InetAddress InetAddress::create(const struct sockaddr *addr) {
+	switch (addr->sa_family)
+	{
+		case AF_INET:
+		{
+			const struct sockaddr_in *addr_in = (const struct sockaddr_in*)addr;
+			return (InetAddress(addr_in->sin_addr.s_addr, addr_in->sin_port));
+		}
+		default:
+			throw Exception("unsupported socket(only AF_INET) family: " + Base::toString(addr->sa_family, 10));
+	}
+}
+
+std::string InetAddress::address(void) const {
+	return (Base::toString((int)_address[0], 10) + "." + Base::toString((int)_address[1], 10) + "." + Base::toString((int)_address[2], 10) + "." + Base::toString((int)_address[3], 10));
+}
--- srcs/address/InetAddress.hpp
+++ srcs/address/InetAddress.hpp
@@ -2,18 +2,25 @@
 #define INETADDRESS_HPP
 
 #include <string>
+#include <sys/socket.h>
+#include <arpa/inet.h>
+#include "../exception/Exception.hpp"
+#include "../util/Base.hpp"
 
 class InetAddress {
 private:
-	int			_port;
-	std::string _address;
+	int				_port;
+	unsigned char 	_address[4];
 public:
 	InetAddress(const InetAddress& other);
+	InetAddress(unsigned int address, int port);
 	InetAddress& operator=(const InetAddress& other);
 	InetAddress(void);
 	~InetAddress(void);
-	int port() const;
-	std::string address() const;
+	int port(void) const;
+	std::string address(void) const;
+public:
+	static InetAddress create(const struct sockaddr* addr);
 };
 
 #endif
