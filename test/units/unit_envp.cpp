33daa32ff0b6ce462c446dd8204e04b69ede815a
seongtaekkim <chxortnl@gmail.com>
2023-05-26 16:37:39 +0900

    add : class diagram

---
+++ test/units/unit_envp.cpp
@@ -0,0 +1,14 @@
+// int main(int argc, char* argv[], char** envp) {
+	
+// 	std::cout << envp[0] << std::endl;
+// 	std::cout << envp[1] << std::endl;
+// 	SEnvironment::setEnv(envp);
+// 	const SEnvironment &environment = SEnvironment::getEnv();
+// 	SEnvironment env = environment;
+
+	
+// 	typedef std::map<std::string, std::string> env_map;
+
+// 	std::cout << env.getOne("USER") << std::endl;
+	
+// }
